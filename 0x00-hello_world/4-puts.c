a4159f6917803df892736d871269a67841bb14ab
Hiba <heeebaaa7@gmail.com>
2023-03-16 13:37:24 -0700

    printing using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ * puts - "prints Programming is like building a multilingual puzzle"
+ *
+ * Return:0 if it prints exactly the string
+ */
+
 int main(void)
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
