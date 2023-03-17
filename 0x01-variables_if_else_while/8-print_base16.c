81ff43b057cdf547adcaa85b75aae0fff3d4acb7
Hiba <heeebaaa7@gmail.com>
2023-03-17 14:32:20 -0700

    numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int l;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (l = 97; l <= 102; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
