48634fb241023e7f11de15631559e9701933f0e0
Hiba <heeebaaa7@gmail.com>
2023-03-21 14:50:15 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,16 @@
  * Return: 0
  */
 
-int main()
+int main(void)
 {
-    char str[] = "_putchar";
-    int i;
+	char str[] = "_putchar";
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        putchar(str[i]);
-    }
-    putchar('\n');
-    return 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+	return (0);
 }
 
