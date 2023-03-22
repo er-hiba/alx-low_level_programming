b4c7080337c94736fcc00f2bfe027ea430f8cff8
Hiba <heeebaaa7@gmail.com>
2023-03-22 11:46:31 -0700

    lowercase alphabet times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,10 @@ void print_alphabet_x10(void)
 {
 	char letter;
 	int i = 1;
-	
-	while(i<=10)
-	{	
-		for(letter = 'a'; letter <= 'z'; letter++)
+
+	while (i <= 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			putchar(letter);
 		}
