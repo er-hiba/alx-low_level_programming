a3c78f9f6ef98834d78410b3002287f1c061c181
Hiba <heeebaaa7@gmail.com>
2023-03-22 13:01:21 -0700

    last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+# include "main.h"
+/**
+ * print_last_digit - X
+ * @n: X
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = n % 10
+
+	_putchar(ld);	
+	return (ld);
+}
